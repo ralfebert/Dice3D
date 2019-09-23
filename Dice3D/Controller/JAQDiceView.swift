4d7d844226798542bd3ffe4c82a188fa26c29e2d
Ralf Ebert <info@ralfebert.de>
2019-09-23 20:44:35 +0200

    cleanup

--- Dice3D/Controller/JAQDiceView.swift
+++ Dice3D/Controller/JAQDiceView.swift
@@ -5,15 +5,15 @@ import SceneKit
 }
 
 class JAQDiceView: SCNView {
+
     weak var diceDelegate: JAQDiceProtocol?
-    @IBInspectable var maximumJumpHeight: CGFloat = 0.0
-    @IBInspectable var squareSizeHeight: CGFloat = 0.0
-    @IBInspectable var cameraPerspective = false
-    var floorImage: UIImage = UIImage(named: "woodTile")!
+    var maximumJumpHeight: CGFloat = 120
+    var squareSizeHeight: CGFloat = 60
+    var floorImage = UIImage(named: "woodTile")!
 
     @IBAction func rollTheDice(_: Any) {
-        self.dice1?.physicsBody?.applyTorque(SCNVector4Make(self.randomJump(), -12, 0, 10), asImpulse: true)
-        self.dice2?.physicsBody?.applyTorque(SCNVector4Make(self.randomJump(), +10, 0, 10), asImpulse: true)
+        self.dice1.physicsBody?.applyTorque(SCNVector4Make(self.randomJump(), -12, 0, 10), asImpulse: true)
+        self.dice2.physicsBody?.applyTorque(SCNVector4Make(self.randomJump(), +10, 0, 10), asImpulse: true)
 
         self.timesStopped = 0
 
@@ -21,9 +21,9 @@ class JAQDiceView: SCNView {
         self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.checkStatus(_:)), userInfo: nil, repeats: true)
     }
 
-    private var dice1: SCNNode?
-    private var dice2: SCNNode?
-    private var camera: SCNNode?
+    private var dice1: SCNNode!
+    private var dice2: SCNNode!
+    private var camera: SCNNode!
     private var timer: Timer?
     private var timesStopped = 0
 
@@ -32,23 +32,16 @@ class JAQDiceView: SCNView {
 
         backgroundColor = UIColor.black
 
-        if self.maximumJumpHeight == 0.0 {
-            self.maximumJumpHeight = 120
-        }
-        if self.squareSizeHeight == 0.0 || self.squareSizeHeight < 30 {
-            self.squareSizeHeight = 60
-        }
-
         self.loadScene()
     }
 
     func loadScene() {
 
         let url = Bundle.main.url(forResource: "Dices", withExtension: "scn")!
-        self.scene = try! SCNScene(url: url)
+        let scene = try! SCNScene(url: url)
 
         self.timesStopped = 0
-        scene?.physicsWorld.gravity = SCNVector3Make(0, -980, 0)
+        scene.physicsWorld.gravity = SCNVector3Make(0, -980, 0)
 
         let floorGeometry = SCNFloor()
         floorGeometry.firstMaterial?.diffuse.contents = self.floorImage
@@ -57,64 +50,62 @@ class JAQDiceView: SCNView {
         let floorNode = SCNNode()
         floorNode.geometry = floorGeometry
         floorNode.physicsBody = SCNPhysicsBody.static()
-        scene?.rootNode.addChildNode(floorNode)
+        scene.rootNode.addChildNode(floorNode)
 
-        self.dice1 = scene?.rootNode.childNode(withName: "Dice_1", recursively: true)
-        self.dice1?.physicsBody = SCNPhysicsBody.dynamic()
+        self.dice1 = scene.rootNode.childNode(withName: "Dice_1", recursively: true)
+        self.dice1.physicsBody = SCNPhysicsBody.dynamic()
 
-        self.dice2 = scene?.rootNode.childNode(withName: "Dice_2", recursively: true)
-        self.dice2?.physicsBody = SCNPhysicsBody.dynamic()
+        self.dice2 = scene.rootNode.childNode(withName: "Dice_2", recursively: true)
+        self.dice2.physicsBody = SCNPhysicsBody.dynamic()
 
         self.camera = SCNNode()
-        self.camera?.camera = SCNCamera()
-        self.camera?.camera?.zFar = Double(self.maximumJumpHeight * 2)
-        self.camera?.eulerAngles = SCNVector3Make(-.pi / 2, 0, 0)
-        self.camera?.position = SCNVector3Make(0, Float(self.maximumJumpHeight - 20), 0)
-        if self.cameraPerspective {
-            self.camera?.rotation = SCNVector4Make(-1, 0, 0, .pi / 3)
-            self.camera?.position = SCNVector3Make(0, Float(self.maximumJumpHeight - 30), 40)
-        }
-        if let camera = camera {
-            scene?.rootNode.addChildNode(camera)
-        }
+        self.camera.camera = SCNCamera()
+        self.camera.camera?.zFar = Double(self.maximumJumpHeight * 2)
+        self.camera.eulerAngles = SCNVector3Make(-.pi / 2, 0, 0)
+        self.camera.rotation = SCNVector4Make(-1, 0, 0, .pi / 3)
+        self.camera.position = SCNVector3Make(0, Float(self.maximumJumpHeight - 30), 40)
+        scene.rootNode.addChildNode(self.camera)
 
         let diffuseLightFrontNode = SCNNode()
         diffuseLightFrontNode.light = SCNLight()
         diffuseLightFrontNode.light?.type = .omni
         diffuseLightFrontNode.position = SCNVector3Make(0, Float(self.maximumJumpHeight), Float(self.maximumJumpHeight / 3))
-        scene?.rootNode.addChildNode(diffuseLightFrontNode)
+        scene.rootNode.addChildNode(diffuseLightFrontNode)
 
         self.placeWalls(in: scene, sizeBox: self.squareSizeHeight)
 
         pointOfView = self.camera
         allowsCameraControl = false
+
+        self.scene = scene
+
     }
 
-    func placeWalls(in scene: SCNScene?, sizeBox size: CGFloat) {
+    func placeWalls(in scene: SCNScene, sizeBox size: CGFloat) {
         let left = SCNNode()
         left.position = SCNVector3Make(Float(-size / 2), Float(size / 2), 0)
         left.geometry = SCNBox(width: 1, height: size, length: size, chamferRadius: 0)
-        scene?.rootNode.addChildNode(left)
+        scene.rootNode.addChildNode(left)
 
         let front = SCNNode()
         front.position = SCNVector3Make(0, Float(size / 2), Float(-size / 2))
         front.geometry = SCNBox(width: size, height: size, length: 1, chamferRadius: 0)
-        scene?.rootNode.addChildNode(front)
+        scene.rootNode.addChildNode(front)
 
         let right = SCNNode()
         right.position = SCNVector3Make(Float(size / 2), Float(size / 2), 0)
         right.geometry = SCNBox(width: 1, height: size, length: size, chamferRadius: 0)
-        scene?.rootNode.addChildNode(right)
+        scene.rootNode.addChildNode(right)
 
         let back = SCNNode()
         back.position = SCNVector3Make(0, Float(size / 2), Float(size / 2))
         back.geometry = SCNBox(width: size, height: size, length: 1, chamferRadius: 0)
-        scene?.rootNode.addChildNode(back)
+        scene.rootNode.addChildNode(back)
 
         let top = SCNNode()
         top.position = SCNVector3Make(0, Float(size), 0)
         top.geometry = SCNBox(width: size, height: 1, length: size, chamferRadius: 0)
-        scene?.rootNode.addChildNode(top)
+        scene.rootNode.addChildNode(top)
 
         self.applyRigidPhysics(left)
         self.applyRigidPhysics(front)
@@ -123,11 +114,9 @@ class JAQDiceView: SCNView {
         self.applyRigidPhysics(top)
     }
 
-    func applyRigidPhysics(_ node: SCNNode?) {
-        if let node = node {
-            node.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: node, options: nil))
-        }
-        node?.opacity = 0.0
+    func applyRigidPhysics(_ node: SCNNode) {
+        node.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: node, options: nil))
+        node.opacity = 0.0
     }
 
     func randomJump() -> Float {
@@ -137,8 +126,8 @@ class JAQDiceView: SCNView {
     }
 
     @objc func checkStatus(_: Any?) {
-        let result = self.dice1?.jaq_rotatedVector().x
-        if ((result ?? 0.0) > 0.95 && (result ?? 0.0) < 1.05) || ((result ?? 0.0) < 0.05 && (result ?? 0.0) > -0.05) || ((result ?? 0.0) > -1.05 && (result ?? 0.0) < -0.95) {
+        let result = self.dice1.jaq_rotatedVector().x
+        if (result > 0.95 && result < 1.05) || (result < 0.05 && result > -0.05) || (result > -1.05 && result < -0.95) {
 
             self.timesStopped += 1
 
@@ -148,7 +137,7 @@ class JAQDiceView: SCNView {
             #endif
             if self.timesStopped > threshold {
                 self.timer?.invalidate()
-                self.diceDelegate?.diceView(self, rolledWithFirstValue: self.dice1?.jaq_boxUpIndex() ?? 0, secondValue: self.dice2?.jaq_boxUpIndex() ?? 0)
+                self.diceDelegate?.diceView(self, rolledWithFirstValue: self.dice1.jaq_boxUpIndex(), secondValue: self.dice2.jaq_boxUpIndex())
             }
         } else {
             self.timesStopped = 0
